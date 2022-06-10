1456dc6973db88e613e75c4dacce1195559eb263
Your Name <you@example.com>
2022-06-10 14:38:28 +0300

    create social media

--- src/components/get-social-media.js
+++ src/components/get-social-media.js
@@ -1,4 +1,12 @@
-// Create 123 component
-const asd = function () {};
+// Create socialMedia component
+const socialMedia = function (href, scr) {
+  return (
+    '<a href="' +
+    href +
+    '" target="_self"><img width="24" alt="instagram" src="./images/' +
+    src +
+    '" style="display: block;" border="0"></a>'
+  );
+};
 
-export {};
+export { socialMedia };
