99b85c91149665af35803d26ada3e6a540ae37bf
Your Name <you@example.com>
2022-06-10 14:31:15 +0300

    create link component

--- src/components/get-link.js
+++ src/components/get-link.js
@@ -1,4 +1,10 @@
-// Create 123 component
-const asd = function () {};
+// Create link component
+const link = function (link) {
+  return (
+    '<a href="' +
+    link +
+    '" data-link-id="960357289" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a>'
+  );
+};
 
-export {};
+export { link };
