53d39b0d8ff11a43fb479886096de3ed36641374
Your Name <you@example.com>
2022-06-10 14:51:03 +0300

    create address component

--- src/components/get-address.js
+++ src/components/get-address.js
@@ -3,6 +3,7 @@ const address = function (address) {
   if (address == "") {
     throw new Error("No address");
   }
+  return '<p style="margin-top: 0px; margin-bottom: 0px;">' + address + "</p>";
 };
 
 export { address };
