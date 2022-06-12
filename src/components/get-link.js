7fac98a449b3ec185156e0b05dc0e8d5b524f30a
Your Name <you@example.com>
2022-06-12 21:57:33 +0300

    add id argument in component

--- src/components/get-link.js
+++ src/components/get-link.js
@@ -1,12 +1,17 @@
 // Create link component
-const link = function (link) {
+const link = function (id, link) {
+  if (id == "") {
+    throw new Error("No id link");
+  }
   if (link == "") {
     throw new Error("No link link");
   }
   return (
     '<a href="' +
     link +
-    '" data-link-id="960357289" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a>'
+    '" data-link-id="' +
+    id +
+    '" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a>'
   );
 };
 
