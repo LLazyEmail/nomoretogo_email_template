8f1978fdd06e90a28a1d07d6e4102446f914eba6
Your Name <you@example.com>
2022-06-14 01:04:27 +0300

    update supportContact

--- src/components/supportContact.js
+++ src/components/supportContact.js
@@ -1,6 +1,29 @@
 // Create supportContact component
+var inlineTab = function (href1, href2, mailHref) {
+  return `<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+  <tbody><tr>
+    <td align="center" style="padding: 0px 40px;" class="mlContentOuter">
+      <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
+        <tbody><tr>
+          <td class="bodyTitle" id="bodyText-36" style="font-family: "Poppins", sans-serif; font-size: 14px; line-height: 150%; color: #6f6f6f;">
+            <h4><strong>No More To-Go Facebook Users:</strong><br></h4>
+            <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;">Connect with other home cooks on our closed group for No More To-Go Members Only <a href="${href1}" data-link-id="960357287" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">Facebook Group Page</a>.</p>
+            <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
+            <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
+            <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
+            <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"><strong>Have Questions?</strong><br>Please contact us via the "<a href="${href2}" data-link-id="960357289" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a> link at the bottom of each page on the site. Or, email us at <a href="mailto:${mailHref}" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">contact@nomoretogo.com.&nbsp;</a><br><strong><br></strong></p>
+            <p style="margin-top: 0px; margin-bottom: 0px; line-height: 150%;"></p>
+          </td>
+        </tr>
+      </tbody></table>
+    </td>
+  </tr>
+</tbody></table>`;
+};
+
 export default function (href1, href2, mailHref) {
-  return `<table align="center" border="0" bgcolor="#ffffff" class="mlContentTable mlContentTableDefault" cellpadding="0" cellspacing="0" width="640">
+  return (
+    `<table align="center" border="0" bgcolor="#ffffff" class="mlContentTable mlContentTableDefault" cellpadding="0" cellspacing="0" width="640">
 <tbody><tr>
   <td class="mlContentTableCardTd">
     <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" class="mlContentTable ml-default" style="width: 640px; min-width: 640px;" width="640">
@@ -10,27 +33,9 @@ export default function (href1, href2, mailHref) {
             <tbody><tr>
               <td height="40" class="spacingHeight-40" style="line-height: 40px; min-height: 40px;"></td>
             </tr>
-          </tbody></table>
-          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
-            <tbody><tr>
-              <td align="center" style="padding: 0px 40px;" class="mlContentOuter">
-                <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
-                  <tbody><tr>
-                    <td class="bodyTitle" id="bodyText-36" style="font-family: "Poppins", sans-serif; font-size: 14px; line-height: 150%; color: #6f6f6f;">
-                      <h4><strong>No More To-Go Facebook Users:</strong><br></h4>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;">Connect with other home cooks on our closed group for No More To-Go Members Only <a href="${href1}" data-link-id="960357287" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">Facebook Group Page</a>.</p>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"><strong>Have Questions?</strong><br>Please contact us via the "<a href="${href2}" data-link-id="960357289" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a> link at the bottom of each page on the site. Or, email us at <a href="mailto:${mailHref}" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">contact@nomoretogo.com.&nbsp;</a><br><strong><br></strong></p>
-                      <p style="margin-top: 0px; margin-bottom: 0px; line-height: 150%;"></p>
-                    </td>
-                  </tr>
-                </tbody></table>
-              </td>
-            </tr>
-          </tbody></table>
-          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+          </tbody></table>` +
+    inlineTab(href1, href2, mailHref) +
+    `<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
             <tbody><tr>
               <td height="20" class="spacingHeight-20" style="line-height: 20px; min-height: 20px;"></td>
             </tr>
@@ -40,5 +45,6 @@ export default function (href1, href2, mailHref) {
     </tbody></table>
   </td>
 </tr>
-</tbody></table>`;
+</tbody></table>`
+  );
 }
