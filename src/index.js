b3fc37b479ab7388b83d85a49cb635ea2112790f
Your Name <you@example.com>
2022-06-13 17:50:56 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // Get components
 import headComponent from "./components/get-headComponent.js";
 import footer from "./components/get-footer.js";
-import ViewInBrows from "./components/get-view-in-brows.js";
-import UnsubLink from "./components/get-unsub-link.js";
+import ViewInBrows from "./miscellaneous/get-view-in-brows.js";
+import UnsubLink from "./miscellaneous/get-unsub-link.js";
 import image from "./components/get-image.js";
 import button from "./components/get-button.js";
 import link from "./components/get-link.js";
 import headings from "./typography/get-headings.js";
-import SocialMedia from "./components/get-social-media.js";
-import address from "./components/get-address.js";
+import SocialMedia from "./miscellaneous/get-social-media.js";
+import address from "./miscellaneous/get-address.js";
 import supportContact from "./components/supportContact.js";
 
 export {
