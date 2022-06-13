7348dc0ac68de0e0158a25b61605a44cda3ad5ac
Your Name <you@example.com>
2022-06-13 12:58:26 +0300

    create headings

--- src/components/get-headings.js
+++ src/components/get-headings.js
@@ -1,4 +1,24 @@
 // Create headings component
-export default function () {
-  return "headings";
+export default function (text, type) {
+  const resault = "";
+  if (type == 1) {
+    resault = "<h1 style='font-size: 2em, font-weight:700'>" + text + "</h1>";
+  }
+  if (type == 2) {
+    resault = "<h2 style='font-size: 1.8em'>" + text + "</h2>";
+  }
+  if (type == 3) {
+    resault = "<h3 style='font-size: 1.6em'>" + text + "</h3>";
+  }
+  if (type == 4) {
+    resault = "<h4 style='font-size: 1.4em'>" + text + "</h4>";
+  }
+  if (type == 5) {
+    resault = "<h5 style='font-size: 1.2em'>" + text + "</h5>";
+  }
+  if (type == 6) {
+    resault = "<h6 style='font-size: 1em'>" + text + "</h6>";
+  }
+
+  return resault;
 }
