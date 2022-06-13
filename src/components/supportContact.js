c788eada8c0fe8f54566e92c4b2c60f2dc7bb5e2
Your Name <you@example.com>
2022-06-13 23:19:03 +0300

    update supportContact

--- src/components/supportContact.js
+++ src/components/supportContact.js
@@ -1,5 +1,5 @@
 // Create supportContact component
-export default function () {
+export default function (href1, href2, mailHref) {
   return `<table align="center" border="0" bgcolor="#ffffff" class="mlContentTable mlContentTableDefault" cellpadding="0" cellspacing="0" width="640">
 <tbody><tr>
   <td class="mlContentTableCardTd">
@@ -18,11 +18,11 @@ export default function () {
                   <tbody><tr>
                     <td class="bodyTitle" id="bodyText-36" style="font-family: "Poppins", sans-serif; font-size: 14px; line-height: 150%; color: #6f6f6f;">
                       <h4><strong>No More To-Go Facebook Users:</strong><br></h4>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;">Connect with other home cooks on our closed group for No More To-Go Members Only <a href="https://click.mailerlite.com/link/c/YT0xOTM0MzU4ODYxNzU0NDA1OTgyJmM9bDhuNSZiPTk2MDM1NzI4NyZkPWk1dDVmNGo=.4xn_5-6HMExtTo3xZ_Ln6RjQkfRky6mhHhF8JlatA5w" data-link-id="960357287" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">Facebook Group Page</a>.</p>
+                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;">Connect with other home cooks on our closed group for No More To-Go Members Only <a href="${href1}" data-link-id="960357287" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">Facebook Group Page</a>.</p>
                       <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
                       <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
                       <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"></p>
-                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"><strong>Have Questions?</strong><br>Please contact us via the "<a href="https://click.mailerlite.com/link/c/YT0xOTM0MzU4ODYxNzU0NDA1OTgyJmM9bDhuNSZiPTk2MDM1NzI4OSZkPXMwZDVyOHA=.8-LFEsk4bUN_iLltde13xeu2Odr968iC9CMsJcRPdTE" data-link-id="960357289" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a> link at the bottom of each page on the site. Or, email us at <a href="mailto:contact@nomoretogo.com.%C2%A0" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">contact@nomoretogo.com.&nbsp;</a><br><strong><br></strong></p>
+                      <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"><strong>Have Questions?</strong><br>Please contact us via the "<a href="${href2}" data-link-id="960357289" target="_blank" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">How Can We Help"</a> link at the bottom of each page on the site. Or, email us at <a href="mailto:${mailHref}" style="word-break: break-word; font-family: "Poppins", sans-serif; color: #09c269; text-decoration: underline;">contact@nomoretogo.com.&nbsp;</a><br><strong><br></strong></p>
                       <p style="margin-top: 0px; margin-bottom: 0px; line-height: 150%;"></p>
                     </td>
                   </tr>
