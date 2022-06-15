70c89d4eac92a0298cef5f4d0866a78a77ff7618
Your Name <you@example.com>
2022-06-16 01:14:12 +0300

    change, update

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // Create config file
 const contact = "xxx";
-const mailingAddress = "xxx";
-const unsubscribe = "xxx";
+const mailingAddress = "contact@nomoretogo.co";
+const unsubscribe =
+  "https://click.mailerlite.com/link/c/YT0xOTM0MzU4ODYxNzU0NDA1OTgyJmM9bDhuNSZiPTk2MDM1NzY2OSZkPWo3eTJlNHY=.Ec_fY2NpMcOTAMs-XIr1n9exawt8fd3IsksWtSJ2kak";
 const pathImages = "../data/images/";
-const pathMainLogo =
-  "../data/images/41075f6aa3320258a8629d833918c6c56fc48e98.jpeg";
+const pathMainLogo = "../data/images/logo.jpeg";
 const pathSocialIcons = "../data/images/";
 
 export {
