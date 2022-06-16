1c2b5dd4e0c0748c9b3f3762eb75fea8ccc83dc0
Your Name <you@example.com>
2022-06-16 15:30:24 +0300

    create topPanelViewInBrows

--- src/outerTemplate/topPanelViewInBrows.js
+++ src/outerTemplate/topPanelViewInBrows.js
@@ -1,5 +1,7 @@
 // Create panel view in brows
 
+import viewInBrowserLink from "../miscellaneous/view-in-browser-link.js";
+
 // Top panel start
 const topPanelViewInBrows = (idViewInBrows, hrefViewInBrows) => {
   return `<table cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mobileHide">
@@ -12,8 +14,8 @@ const topPanelViewInBrows = (idViewInBrows, hrefViewInBrows) => {
           <tr>
             <td align="left" style="font-family: 'Poppins', sans-serif; color: #111111; font-size: 12px; line-height: 18px;"></td>
             <td align="right" style="font-family: 'Poppins', sans-serif; color: #111111; font-size: 12px; line-height: 18px;">
-              <a style="color: #111111;" href="${hrefViewInBrows}" data-link-id="${idViewInBrows}" target="_blank">View in browser</a>
-            </td>
+              ${viewInBrowserLink(idViewInBrows, hrefViewInBrows)}
+              </td>
           </tr>
           <tr>
             <td colspan="2" height="20"></td>
