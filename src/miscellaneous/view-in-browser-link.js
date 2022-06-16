178da5cc0de3dd549cb9b7e482afc89a04e56d07
Your Name <you@example.com>
2022-06-16 15:26:07 +0300

    reorganization viewInBrowsLink

--- src/miscellaneous/view-in-browser-link.js
+++ src/miscellaneous/view-in-browser-link.js
@@ -1,16 +1,18 @@
 // Create view-in-brows component
 
 // View in brows link start
-const viewInBrowsLink = (idViewInBrows, hrefViewInBrows) => {};
+const viewInBrowsLink = (idViewInBrows, hrefViewInBrows) => {
+  return `<a style="color: #111111;" href="${hrefViewInBrows}" data-link-id="${idViewInBrows}" target="_blank">View in browser</a>`;
+};
 // View in brows link end
 
 export default function (idViewInBrows, hrefViewInBrows) {
   if (idViewInBrows == "") {
-    throw new Error("No idViewInBrows topPanelViewInBrows");
+    throw new Error("No idViewInBrows viewInBrowsLink");
   }
   if (hrefViewInBrows == "") {
-    throw new Error("No hrefViewInBrows topPanelViewInBrows");
+    throw new Error("No hrefViewInBrows viewInBrowsLink");
   }
 
-  return topPanelViewInBrows(idViewInBrows, hrefViewInBrows);
+  return viewInBrowsLink(idViewInBrows, hrefViewInBrows);
 }
