7f7758a05582f413b1e56531b9046582e29c7390
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-19 22:37:47 +0300

    Update displayBody.js

--- src/display/displayBody.js
+++ src/display/displayBody.js
@@ -1,6 +1,14 @@
 import displayFactoryTwo from './factoryTwo';
 
+let addon1 = {
+    footer: footerString,
 
+    logoTop:logoTopComponent(), 
+    logoBottom: logoBottomComponent(), 
+    content:'[[THIS IS PLACE FOR A CONTENT INSIDE]',
+    previewText:previewTextComponent('[AMA PREVIEW TEXT]')
+
+}
 //variant one
 const settings = {
     component: '',
