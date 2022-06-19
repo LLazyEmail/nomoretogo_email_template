9c1218895dc6946c8ddbfb2e12529f94657d30a9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-19 22:50:58 +0000

    clean up

--- src/display/displayBody.js
+++ src/display/displayBody.js
@@ -1,12 +1,12 @@
 import displayFactoryTwo from './factoryTwo';
 
 let addon1 = {
-    footer: footerString,
+    // footer: footerString,
 
-    logoTop:logoTopComponent(), 
-    logoBottom: logoBottomComponent(), 
-    content:'[[THIS IS PLACE FOR A CONTENT INSIDE]',
-    previewText:previewTextComponent('[AMA PREVIEW TEXT]')
+    // logoTop:logoTopComponent(), 
+    // logoBottom: logoBottomComponent(), 
+    // content:'[[THIS IS PLACE FOR A CONTENT INSIDE]',
+    // previewText:previewTextComponent('[AMA PREVIEW TEXT]')
 
 }
 //variant one
