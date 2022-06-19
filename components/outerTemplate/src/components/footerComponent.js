b784ee79031687f793f6d4e19eef1d72314d669c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-20 02:01:53 +0300

    Update footerComponent.js

--- components/outerTemplate/src/components/footerComponent.js
+++ components/outerTemplate/src/components/footerComponent.js
@@ -9,9 +9,11 @@ export default function (footContent) {
   }
 
   if (footContent.length == 0) {
+    // @TODO we can do it as one check or rename the output message
     throw new Error("No footContent footer");
   }
 
+  //@TODO move this loop away, it's hard to wrap your head around two long strings
   var res = footContent.map((cont) => {
     const srcPath = pathSocialIcons + cont.src;
     return `<td align="center" width="24" style="padding: 0px 5px;" ng-show="slink.link != ''">
