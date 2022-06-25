f48ae2847de6502ffb5a7c6700dd1b56a9fbabaa
Your Name <you@example.com>
2022-06-26 01:06:42 +0300

    update Footer component

--- components/outerTemplate/src/components/footerComponent.js
+++ components/outerTemplate/src/components/footerComponent.js
@@ -9,8 +9,8 @@ function createPathToImage(src) {
 }
 // Create path to image end
 
-// Create foot conten start
-function createFootContent(footContent) {
+// Create foot content start
+function createFootItem(footContent) {
   return footContent.map((cont) => {
     return `<td align="center" width="24" style="padding: 0px 5px;" ng-show="slink.link != ''">
     <a href="${cont.href}" target="_self">
@@ -21,14 +21,20 @@ function createFootContent(footContent) {
   </td>`;
   });
 }
-// Create foot conten end
+// Create foot content end
+
+// Create foot content start
+function createFootContent(footContent) {
+  return `<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left">
+  <tbody><tr>${createFootItem(footContent)}</tr></tbody>
+</table>`;
+}
+// Create foot content end
 
 export default function (footContent) {
   if (footContent == "") {
     throw new Error("No footContent footer");
   }
 
-  return `<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left">
-  <tbody><tr>${createFootContent(footContent)}</tr></tbody>
-</table>`;
+  createFootContent(footContent);
 }
