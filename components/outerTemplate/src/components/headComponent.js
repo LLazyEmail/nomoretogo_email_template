5680e01a3cb08dba4cb881f172ed5998792b03d7
Your Name <you@example.com>
2022-06-25 22:56:37 +0300

    update headComponent

--- components/outerTemplate/src/components/headComponent.js
+++ components/outerTemplate/src/components/headComponent.js
@@ -9,6 +9,8 @@ import font2 from "./font2.js";
 import meta from "./headMetaTags";
 import ifStyle from "./ifStyle.js";
 
+const HEAD_COMPONENT_ERROR = `Empty ${variable} in headComponent`;
+
 // TODO let's make this file more clear.
 // imports of all variables we'll move outside and our function will just accept an object with all these variables.
 // and we need to put more checks and errors here.
@@ -33,11 +35,51 @@ function createHeadComponent(data) {
 }
 // Create head component end
 
-export default function (title) {
-  if (title == "") {
-    throw new Error("No title");
+function searchForErrors(data) {
+  var variable = "";
+  if (data.blobHead == "") {
+    variable = "blobHead";
+    throw new Error(HEAD_COMPONENT_ERROR);
   }
+  if (data.bodyStyle == "") {
+    variable = "bodyStyle";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.mediaStyle == "") {
+    variable = "mediaStyle";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.style == "") {
+    variable = "style";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.style2 == "") {
+    variable = "style2";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.font1 == "") {
+    variable = "font1";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.font2 == "") {
+    variable = "font2";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.meta == "") {
+    variable = "meta";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.ifStyle == "") {
+    variable = "ifStyle";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+  if (data.title == "") {
+    variable = "title";
+    throw new Error(HEAD_COMPONENT_ERROR);
+  }
+}
 
+export default function (title) {
   const data = {
     blobHead,
     bodyStyle,
@@ -51,5 +93,6 @@ export default function (title) {
     title,
   };
 
+  searchForErrors(data);
   createHeadComponent(data);
 }
