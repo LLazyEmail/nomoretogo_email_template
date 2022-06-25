db734411c554482b3d932ee15fea7740e5d5bdea
Your Name <you@example.com>
2022-06-26 00:53:44 +0300

    update #77

--- components/outerTemplate/src/components/footerComponent.js
+++ components/outerTemplate/src/components/footerComponent.js
@@ -9,37 +9,26 @@ function createPathToImage(src) {
 }
 // Create path to image end
 
+// Create foot conten start
+function createFootContent(footContent) {
+  return footContent.map((cont) => {
+    return `<td align="center" width="24" style="padding: 0px 5px;" ng-show="slink.link != ''">
+    <a href="${cont.href}" target="_self">
+    <img width="24" alt="${cont.alt}" 
+    src="${createPathToImage(cont.src)}" 
+    style="display: block; border="0">
+    </a>
+  </td>`;
+  });
+}
+// Create foot conten end
+
 export default function (footContent) {
   if (footContent == "") {
     throw new Error("No footContent footer");
   }
 
-  if (footContent.length == 0) {
-    // TODO we can do it as one check or rename the output message
-    throw new Error("Array footContent is empty footer");
-  }
-
-  // TODO move this loop away, it's hard to wrap your head around two long strings
-  var res = footContent.map((cont) => {
-    // TODO make srcPath as a function that returns a full string.
-    // reason is simple, pathSocialIcons is a variable from the outside
-    // and cont.src is a variable that was passed into this method from the outside as an array of settings.
-    // sooo - it's very confusing might be
-
-    return `<td align="center" width="24" style="padding: 0px 5px;" ng-show="slink.link != ''">
-      <a href="${cont.href}" target="_self">
-      <img width="24" alt="${cont.alt}" 
-      src="${createPathToImage(cont.src)}" 
-      style="display: block; border="0">
-      </a>
-    </td>`;
-  });
-
-  return (
-    `<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left">
-  <tbody><tr>` +
-    res +
-    `</tr></tbody>
-</table>`
-  );
+  return `<table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left">
+  <tbody><tr>${createFootContent(footContent)}</tr></tbody>
+</table>`;
 }
