c808d050bd938f54d344418ea95683b665f6dba0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-03 15:01:02 +0300

    Update displayTopPanelViewInBrows.js

--- src/display/displayTopPanelViewInBrows.js
+++ src/display/displayTopPanelViewInBrows.js
@@ -1,5 +1,8 @@
 import topPanelViewInBrows from '../domain/outer/topPanelViewInBrows';
 
+// TODO Vadim not sure why we need a try statement here.
+// looks strange
+
 try {
   var fullComponent = topPanelViewInBrows(1, 'href');
 } catch (err) {
