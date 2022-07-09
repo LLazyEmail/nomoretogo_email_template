9c88985fabd56e74b525badd14fc095319ae0f27
Your Name <you@example.com>
2022-07-10 02:23:42 +0300

    create supportContact

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,22 +1,31 @@
 // import HeadString from '../src/display/displayHead';
 // import ContentMainTableWrapString from '../src/display/displayContentMainTableWrap';
 // import contentString from '../src/display/displayContent';
-import instructionString from '../src/display/displayInstruction';
+// import instructionString from '../src/display/displayInstruction';
+import SupportContactString from '../src/display/displaySupportContact';
 // import FooterString from '../src/display/displayFooter';
 
 describe('test fullComponent', () => {
   // test('rendering HeadString Component', () => {
   //   console.log(HeadString);
   // });
+
   // test('rendering ContentMainTableWrap Component', () => {
   //   console.log(ContentMainTableWrapString);
   // });
+
   // test('rendering Content Component', () => {
   //   console.log(contentString);
   // });
-  test('rendering Instruction Component', () => {
-    console.log(instructionString);
+
+  // test('rendering Instruction Component', () => {
+  //   console.log(instructionString);
+  // });
+
+  test('rendering SupportContact Component', () => {
+    console.log(SupportContactString);
   });
+
   // test('rendering FooterString Component', () => {
   //   console.log(FooterString);
   // });
