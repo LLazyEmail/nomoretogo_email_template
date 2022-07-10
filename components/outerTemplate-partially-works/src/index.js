be0b02b8923408e572ad61d474c3f4c223fd5710
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-10 12:34:42 +0000

    installing outter tmpl from the outside

--- components/outerTemplate-partially-works/src/index.js
+++ components/outerTemplate-partially-works/src/index.js
@@ -1,17 +1,30 @@
 // Get components
 
+
+import blobHead from "./components/blobHead";
+import bodyStyle from "./components/bodyStyle";
+import font1 from "./components/font1";
+import font2 from "./components/font2";
+import headMetaTags from "./components/headMetaTags";
+import ifStyle from "./components/ifStyle";
+import mediaStyle from "./components/mediaStyle";
+import style from "./components/style";
+import style2 from "./components/style2";
+
 // Head sub components start
-import {
-  blobHead,
-  bodyStyle,
-  font1,
-  font2,
-  headMetaTags,
-  ifStyle,
-  mediaStyle,
-  style,
-  style2,
-} from "./components/subHeadComponents.j-s";
+// import {
+//   blobHead,
+//   bodyStyle,
+//   font1,
+//   font2,
+//   headMetaTags,
+//   ifStyle,
+//   mediaStyle,
+//   style,
+//   style2,
+// } from "./components/subHeadComponents.j-s";
+
+
 // Head sub components end
 
 // import blobHead from "./components/blobHead";
@@ -24,10 +37,10 @@ import {
 // import mediaStyle from "./components/mediaStyle";
 // import style from "./components/style";
 // import style2 from "./components/style2";
-import footerComponent from "./components/footerComponent.j-s";
-import headComponent from "./components/headComponent.j-s";
-import tableHeaderBlockLogo from "./components/tableHeaderBlockLogo";
-import topPanelViewInBrows from "./components/topPanelViewInBrows";
+// import footerComponent from "./components/footerComponent.j-s";
+// import headComponent from "./components/headComponent.j-s";
+// import tableHeaderBlockLogo from "./components/tableHeaderBlockLogo";
+// import topPanelViewInBrows from "./components/topPanelViewInBrows";
 
 export {
   blobHead,
@@ -39,8 +52,9 @@ export {
   mediaStyle,
   style,
   style2,
-  footerComponent,
-  headComponent,
-  tableHeaderBlockLogo,
-  topPanelViewInBrows,
+
+  // footerComponent,
+  // headComponent,
+  // tableHeaderBlockLogo,
+  // topPanelViewInBrows,
 };
