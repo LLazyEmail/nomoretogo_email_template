f8d968db04e5351bde066c03e80b6b3b2e7a121b
Your Name <you@example.com>
2022-07-18 20:34:04 +0300

    fix test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,41 +1,41 @@
 import HeadString from '../src/display/displayHead';
-// import ContentMainTableWrapString from '../src/display/displayContentMainTableWrap';
-// import contentString from '../src/display/displayContent';
-// import instructionString from '../src/display/displayInstruction';
-// import SupportContactString from '../src/display/displaySupportContact';
-// import ImagwAfterSupporComponentString from '../src/display/displayImagwAfterSupportComponent';
-// import FooterString from '../src/display/displayFooter';
+import ContentMainTableWrapString from '../src/display/displayContentMainTableWrap';
+import contentString from '../src/display/displayContent';
+import instructionString from '../src/display/displayInstruction';
+import SupportContactString from '../src/display/displaySupportContact';
+import ImageAfterSupporComponentString from '../src/display/displayImageAfterSupportComponent';
+import FooterString from '../src/display/displayFooter';
 ///////////////////////////////////////////////////////////
-// import BodyComponentString from '../src/display/displayBodyComponent';
-import tempTest from '../src/display/displayTempTest';
+import BodyComponentString from '../src/display/displayBodyComponent';
+// import tempTest from '../src/display/displayTempTest';
 
 describe('test fullComponent', () => {
-  // test('rendering HeadString Component', () => {
-  //   console.log(HeadString);
-  // });
-  // test('rendering ContentMainTableWrap Component', () => {
-  //   console.log(ContentMainTableWrapString);
-  // });
-  // test('rendering Content Component', () => {
-  //   console.log(contentString);
-  // });
-  // test('rendering Instruction Component', () => {
-  //   console.log(instructionString);
-  // });
-  // test('rendering SupportContact Component', () => {
-  //   console.log(SupportContactString);
-  // });
-  // test('rendering ImagwAfterSupporComponent Component', () => {
-  //   console.log(ImagwAfterSupporComponentString);
-  // });
-  // test('rendering FooterString Component', () => {
-  //   console.log(FooterString);
-  // });
-  ///////////////////////////////////////////////////////////
-  // test('rendering  BodyString Component', () => {
-  //   console.log(BodyComponentString);
-  // });
-  test('rendering tempTest Component', () => {
-    console.log(tempTest);
+  test('rendering HeadString Component', () => {
+    console.log(HeadString);
   });
+  test('rendering ContentMainTableWrap Component', () => {
+    console.log(ContentMainTableWrapString);
+  });
+  test('rendering Content Component', () => {
+    console.log(contentString);
+  });
+  test('rendering Instruction Component', () => {
+    console.log(instructionString);
+  });
+  test('rendering SupportContact Component', () => {
+    console.log(SupportContactString);
+  });
+  test('rendering ImageAfterSupporComponent Component', () => {
+    console.log(ImageAfterSupporComponentString);
+  });
+  test('rendering FooterString Component', () => {
+    console.log(FooterString);
+  });
+  ///////////////////////////////////////////////////////////
+  test('rendering  BodyString Component', () => {
+    console.log(BodyComponentString);
+  });
+  // test('rendering tempTest Component', () => {
+  //   console.log(tempTest);
+  // });
 });
