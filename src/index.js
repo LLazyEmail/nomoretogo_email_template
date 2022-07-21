4a42496ecea9463255512703191c4dc1da6705f6
Your Name <you@example.com>
2022-07-21 14:02:21 +0300

    fix error after merge

--- src/index.js
+++ src/index.js
@@ -9,22 +9,8 @@ import recipeComponent from './components/recipeComponent';
 import supportComponent from './components/supportComponent';
 import tableWithTwoRecipes from './components/tableWithTwoRecipes';
 
-// export {
-//   tableWithTwoRecipes,
-//   buttonInContentComponent,
-//   contentComponent,
-//   contentMainTableWrap,
-//   imageAfterSupportComponent,
-//   instructionComponent,
-//   lineBlock,
-//   recipeComponent,
-//   supportComponent,
-//   tableWithTwoRecipes,
-// };
-
 export {
   tableWithTwoRecipes,
-<<<<<<< test
   buttonInContentComponent,
   contentComponent,
   contentMainTableWrap,
@@ -34,32 +20,46 @@ export {
   recipeComponent,
   supportComponent,
   tableWithTwoRecipes,
-=======
-  // inner tamplate start
-  // contentMainTableWrap,
-  // inner tamplate end
-
-  // outerTemplate start
-  blobHead,
-  bodyStyle,
-  font1,
-  font2,
-  //--------------
-  // footerComponent,
-  headComponent,
-  headMetaTags,
-  ifStyle,
-  mediaStyle,
-  style,
-  style2,
-  tableHeaderBlockLogo,
-  topPanelViewInBrows,
-  // outerTamplate end
-
-  // typography start
-  // headings,
-  // paragraph,
-  // strong,
-  // typography end
->>>>>>> main
 };
+
+// export {
+//   tableWithTwoRecipes,
+// <<<<<<< test
+//   buttonInContentComponent,
+//   contentComponent,
+//   contentMainTableWrap,
+//   imageAfterSupportComponent,
+//   instructionComponent,
+//   lineBlock,
+//   recipeComponent,
+//   supportComponent,
+//   tableWithTwoRecipes,
+// =======
+//   // inner tamplate start
+//   // contentMainTableWrap,
+//   // inner tamplate end
+
+//   // outerTemplate start
+//   blobHead,
+//   bodyStyle,
+//   font1,
+//   font2,
+//   //--------------
+//   // footerComponent,
+//   headComponent,
+//   headMetaTags,
+//   ifStyle,
+//   mediaStyle,
+//   style,
+//   style2,
+//   tableHeaderBlockLogo,
+//   topPanelViewInBrows,
+//   // outerTamplate end
+
+//   // typography start
+//   // headings,
+//   // paragraph,
+//   // strong,
+//   // typography end
+// >>>>>>> main
+// };
