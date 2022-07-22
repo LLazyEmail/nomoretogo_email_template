47c725cf4d63929e99018baf41a2ce023cdc2eaa
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-22 17:04:15 +0300

    Update displayContent.js

--- src/display/displayContent.js
+++ src/display/displayContent.js
@@ -1,13 +1,20 @@
 // import contentComponent from '../components/contentComponent';
+
+
 // // title
 // const idtitle = '123';
 // const hrefTitle = '123';
 // const textTitle = '123';
+
+
 // // sub title
 // const subTitle = '123';
+
 // // text description
 // const textTop = '123';
 // const textBottom = '123';
+
+
 // // Recipe one
 // const dataImageOne = {
 //   id: '123',
@@ -16,6 +23,8 @@
 //   title: '123',
 //   text: '123',
 // };
+
+
 // // Recipe two
 // const dataImageTwo = {
 //   id: '123',
@@ -24,10 +33,14 @@
 //   title: '123',
 //   text: '123',
 // };
+
+
 // // Button
 // const id = '123';
 // const href = '123';
 // const text = '123';
+
+
 
 // try {
 //   var fullComponent = contentComponent(
@@ -47,10 +60,6 @@
 //   var error = err;
 // }
 
-// var result = error != undefined ? error : fullComponent;
-
-// export default result;
-///////////////////////////////////////////////////////////////
 
 import contentComponent from '../components/contentComponent';
 import createComponent from './createComponentUniversal';
