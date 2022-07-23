0034c046dc1c79226c61f3ec7bed9959b84881f4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-24 02:55:41 +0300

    Update displayContentMainTableWrap.js

--- src/display/displayContentMainTableWrap.js
+++ src/display/displayContentMainTableWrap.js
@@ -11,20 +11,7 @@
 // var error = '';
 // var result = null;
 // try {
-// var params = {
-//   blobHead: blobHead(),
-//   bodyStyle: bodyStyle(),
-//   font1: font1(),
-//   font2: font2(),
-//   headMetaTags: headMetaTags(),
-//   ifStyle: ifStyle(),
-//   mediaStyle: mediaStyle(),
-//   style: style(),
-//   style2: style2(),
 
-//   // variable:value,
-//   title: 'text title',
-// };
 
 //   const factory = new Factory(contentMainTableWrap, params);
 //   result = factory.create();
@@ -32,11 +19,6 @@
 //   error = e.message;
 // }
 
-// if (error != '') {
-//   console.log(error);
-// }
-
-// export default result;
 
 //////////////////////////////////////////////////////////////////
 import createComponent from './createComponentUniversal';
