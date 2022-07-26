5ed05dcac0670bb5af0167c27b27f70b1e54725e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-26 16:15:57 +0000

    upd

--- src/display/displayContent.js
+++ src/display/displayContent.js
@@ -36,16 +36,16 @@ const contentRecipeParams = [
   {
     id: "imageBlock-26",
     href: "https://www.nomoretogo.com/weekly-menu/",
-    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe1.jpeg"
+    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe1.jpeg",
     //------
     title: "Chipotle Cream Skillet Chicken",
-    text: "over Rice and Snap Peas"',
+    text: "over Rice and Snap Peas",
 
   },
   {
     id: "imageBlock-21",
     href: "https://www.nomoretogo.com/weekly-menu/",
-    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe2.jpeg"
+    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe2.jpeg",
     //------
     title: "Pork Schnitzel",
     text: "with German Spaetzle and Green Beans",
@@ -54,7 +54,7 @@ const contentRecipeParams = [
   {
     id: "imageBlock-27",
     href: "https://www.nomoretogo.com/weekly-menu/",
-    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe3.jpeg"
+    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe3.jpeg",
     //------
     title: "Bahn Mi Meatball Skewers",
     text: 'yummy',
@@ -63,7 +63,7 @@ const contentRecipeParams = [
   {
     id: "imageBlock-15",
     href: "https://www.nomoretogo.com/weekly-menu/",
-    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe4.jpeg"
+    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe4.jpeg",
     //------
     title: "Korean Barbecue Beef with Sesame Rice Noodles",
     text: "and Spicy Pickled Cucumbers and Carrots",
@@ -72,7 +72,7 @@ const contentRecipeParams = [
   {
     id: "imageBlock-14",
     href: "https://www.nomoretogo.com/weekly-menu/",
-    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe5.jpeg"
+    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe5.jpeg",
     //------
     title: "Grilled Salmon with Chipotle Cream Sauce",
     text: "over Potato Poblano Hash and Broccolini",
@@ -81,7 +81,7 @@ const contentRecipeParams = [
   {
     id: "imageBlock-20",
     href: "https://www.nomoretogo.com/weekly-menu/",
-    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe6.jpeg"
+    src: "https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/recipe6.jpeg",
     //------
     title: "Saucy Grilled Shrimp",
     text: "and a Corn and Radish Salad with Avocado Dressing",
