ed9632a6f024972671df4ba4c5287765b67c723c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-27 22:14:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,32 +23,3 @@ export {
   supportComponent,
   tableWithTwoRecipes,
 };
-
-// <<<<<<< HEAD
-// import bodyComponent from './components/bodyComponent';
-// =======
-// import { bodyComponent } from 'nmtg-template-mailerlite-outertemplate'
-// import { recipeComponent }  from 'nmtg-template-mailerlite-inner';
-
-// import buttonInContentComponent from './components/buttonInContentComponent';
-// >>>>>>> b397a16ebea6c3c4790519ee56320e7be6d679f4
-// import contentComponent from './components/contentComponent';
-// import contentMainTableWrap from './components/contentMainTableWrap';
-// import imageAfterSupportComponent from './components/imageAfterSupportComponent';
-// import instructionComponent from './components/instructionComponent';
-// import lineBlock from './components/lineBlock';
-
-// import supportComponent from './components/supportComponent';
-// import tableWithTwoRecipes from './components/tableWithTwoRecipes';
-
-// export {
-//   bodyComponent,
-//   contentComponent,
-//   contentMainTableWrap,
-//   imageAfterSupportComponent,
-//   instructionComponent,
-//   lineBlock,
-//   recipeComponent,
-//   supportComponent,
-//   tableWithTwoRecipes,
-// };
