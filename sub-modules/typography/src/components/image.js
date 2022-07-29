06e3866deeeb6dbd5b71b88caf631f0c4b4707e6
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-29 20:03:00 +0000

    update

--- sub-modules/typography/src/components/image.js
+++ sub-modules/typography/src/components/image.js
@@ -1,36 +1,63 @@
-// Create image component
-
 // Error
 import Errors from './Errors';
-
 // Config file
 import { pathToImages } from '../config.js';
 
-// Create path to image start
+const createImage = (params) => {
+  const { src, width } = params;
+  return `<img src="${src}" border="0" alt="" width="${width}" style="display: block;">`;
+};
+
 function createPathToImage(src) {
   return pathToImages + src;
 }
-// Create path to image end
-
-const img2 = () => {
-
-}
-
 
 // Main image block start
-const mainBlock = (params) => {
+const mainBlock = (id, href, src ) => {
 
-  const image = createPathToImage(src);
+  // const imagePath = ;
 
-  return `<a href="${href}" data-link-id="${id}" target="_self">
-      <img 
-        src="${image}" 
-        border="0" alt="" 
-        width="267" style="display: block;">
-    </a>`;
+  const obj = {
+    src: createPathToImage(src),
+    width: 267 // TODO check if width must be string
+  }
+  const imageComponent = createImage(obj); 
+
+
+  return `<a href="${href}" data-link-id="${id}" target="_self">${imageComponent}</a>`;
+
+    //   <img 
+    //     src="${image}" 
+    //     border="0" alt="" 
+    //     width="267" style="display: block;">
+    // </a>`;
+
 };
 
 // Main image block end
+
+const imgWrapper = (params) => {
+  const { id, href, src } = params;
+
+  const error = Errors('image');
+
+  if (id == '') {
+    error.add('No id');
+  }
+  if (href == '') {
+    error.add('No href');
+  }
+  if (src == '') {
+    error.add('No src');
+  }
+
+  return mainBlock(id, href, src);
+}
+
+export {
+  createImage,
+  imgWrapper
+}
 
 export default function (params) {
   const { id, href, src } = params;
