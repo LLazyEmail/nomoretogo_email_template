2009a37a77ae6875a23446e056f74517673342a0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-02 22:01:18 +0000

    updates

---
+++ sub-modules/innerTemplate/src/components/contentSectionOne.js
@@ -0,0 +1,11 @@
+// This section created for displaying title, subtitle, and weekly menu message
+
+const sectionOneComponent = (params) => {
+    // const {} = params;
+
+
+    return ``;
+
+}
+
+export default sectionOneComponent;
---
+++ sub-modules/innerTemplate/src/components/contentSectionTwo.js
@@ -0,0 +1,15 @@
+// This section related to displaying weekly menu recipes
+
+const sectionTwoComponent = (params) => {
+
+    // const {} = params;
+
+
+    return ``;
+
+
+
+
+}
+
+export default sectionTwoComponent;
