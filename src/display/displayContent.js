936883fc0ab1e5ee4d2949a3c18ca20ee3f88f34
Your Name <you@example.com>
2022-08-03 19:07:54 +0300

    save2

--- src/display/displayContent.js
+++ src/display/displayContent.js
@@ -109,4 +109,4 @@ const contentButtonParams = {
 // }
 /* ******************** CREATE CONTENT END ****************************** */
 
-export default 123456789; // result;
+export default 1; // result;
