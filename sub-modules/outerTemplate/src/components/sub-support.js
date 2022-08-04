3db5628d9e3992a8f1e25acff7a29f0b88932db7
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-04 03:47:29 +0300

    Update sub-support.js

--- sub-modules/outerTemplate/src/components/sub-support.js
+++ sub-modules/outerTemplate/src/components/sub-support.js
@@ -1,1 +1,11 @@
-xxx
+{/* <p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;">
+${createSupportTitle(bottomTitle)}
+<br>
+${createSupportTextBottom(
+  bottomLinkHref,
+  bottomLinkId,
+  bottomLinkText,
+  bottomTextContact
+)}
+<br><strong><br></strong>
+</p> */}
