be1c148d5b2d21822f90d9214f30989ab7e4210d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-04 16:59:19 +0000

    debuggin

--- sub-modules/outerTemplate/src/components/footerComponent.js
+++ sub-modules/outerTemplate/src/components/footerComponent.js
@@ -3,10 +3,9 @@ import {
   paragraphComponent,
   linkComponent,
   separatorComponent,
-  imageComponent,
+  // imageComponent,
 } from 'nmtg-template-mailerlite-typography';
 
-// import { emptyBlock } from '../index';
 
 import Errors from './Errors';
 const error = new Errors('footerComponent');
@@ -25,7 +24,6 @@ import { pathSocialIcons, unsubscribe } from '../config';
 const title = 'No More To-Go';
 
 // Address
-// TODO add paragraph component
 const address = () => {
   const addressParams = {
     attributes: `style="margin-top: 0px; margin-bottom: 0px;"`,
@@ -56,114 +54,113 @@ const createUnsubscribe = (href) => {
 
   const linkParams = {
     attributes: `href="${href}" style="color: #111111; text-decoration: underline;"`,
-    content: `<span style="color: #111111;">Unsubscribe</span>`,
+    content: `<span style="color: #111111;">Unsubscribe</span>`, // TODO we can add span into headings
   };
   return linkComponent(linkParams);
-  //-------------------------
-
-  // return `<a href="${href}" style="color: #111111; text-decoration: underline;">
-  //   <span style="color: #111111;">Unsubscribe</span>
-  //   </a>`;
 };
+
+
+const abc = () => {
+return `<table align="center" border="0" bgcolor="#ffffff" class="mlContentTable mlContentTableFooterDefault" cellpadding="0" cellspacing="0" width="640">
+
+<tbody><tr>
+  <td class="mlContentTableFooterCardTd">
+    <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" class="mlContentTable ml-default" style="width: 640px; min-width: 640px;" width="640">
+      <tbody><tr>
+        <td>
+          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+            <tbody><tr>
+              <td height="10" class="spacingHeight-10" style="line-height: 10px; min-height: 10px;"></td>
+            </tr>
+          </tbody></table>
+          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+            <tbody><tr>
+              <td align="center" style="padding: 0px 40px;" class="mlContentOuter">
+                <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
+                  <tbody><tr>
+                    <td align="left" class="bodyTitle" style="font-family: 'Poppins', sans-serif; font-size: 14px; font-weight: 700; line-height: 150%; color: #111111;">${title}</td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+          </tbody></table>
+          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+            <tbody><tr>
+              <td height="10" class="spacingHeight-10"></td>
+            </tr>
+          </tbody></table>
+          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+            <tbody><tr>
+              <td align="center" style="padding: 0px 40px;" class="mlContentOuter">
+                <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
+                  <tbody><tr>
+                    <td align="center">
+                      <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left" width="267" style="width: 267px; min-width: 267px;" class="mlContentTable marginBottom">
+                        <tbody><tr>
+                          <td align="left" class="bodyTitle" id="footerText-44" style="font-family: 'Poppins', sans-serif; font-size: 12px; line-height: 150%; color: #111111;">
+                            ${address()}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td height="25" class="spacingHeight-20"></td>
+                        </tr>
+                        <tr>
+                          <td align="center">
+                            <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left">
+                              <tbody>
+                              <tr>
+                              ${socialPanel}
+                              </tr>
+                            </tbody></table>
+                          </td>
+                        </tr>
+                      </tbody></table>
+                      <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="right" width="267" style="width: 267px; min-width: 267px;" class="mlContentTable">
+                        <tbody><tr>
+                          <td align="right" class="bodyTitle" id="footerUnsubscribeText-44" style="font-family: 'Poppins', sans-serif; font-size: 12px; line-height: 150%; color: #111111;">
+                            ${description()}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td height="10"></td>
+                        </tr>
+                        <tr>
+                          <td align="right" class="bodyTitle" style="font-family: 'Poppins', sans-serif; font-size: 12px; line-height: 150%; color: #111111;">
+                           ${createUnsubscribe(unsubscribe)}
+                          </td>
+                        </tr>
+                      </tbody></table>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+          </tbody></table>
+          <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
+            <tbody><tr>
+              <td height="40" class="spacingHeight-40" style="line-height: 40px; min-height: 40px;"></td>
+            </tr>
+          </tbody></table>
+        </td>
+      </tr>
+    </tbody></table>
+  </td>
+</tr>
+</tbody></table>`;
+
+};
+
 
 // Create foot content start
 const footerComponent = (params) => {
 
   const { amazonFreshBlock, supportBlock, socialPanel } = params;
 
+  const line = supportBlock + separatorComponent() + amazonFreshBlock + separatorComponent() ;
 
-  return 
-  supportBlock + 
-  separatorComponent() + 
-  amazonFreshBlock + 
-    separatorComponent() + 
-  `<table align="center" border="0" bgcolor="#ffffff" class="mlContentTable mlContentTableFooterDefault" cellpadding="0" cellspacing="0" width="640">
 
-  <tbody><tr>
-    <td class="mlContentTableFooterCardTd">
-      <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" class="mlContentTable ml-default" style="width: 640px; min-width: 640px;" width="640">
-        <tbody><tr>
-          <td>
-            <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
-              <tbody><tr>
-                <td height="10" class="spacingHeight-10" style="line-height: 10px; min-height: 10px;"></td>
-              </tr>
-            </tbody></table>
-            <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
-              <tbody><tr>
-                <td align="center" style="padding: 0px 40px;" class="mlContentOuter">
-                  <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
-                    <tbody><tr>
-                      <td align="left" class="bodyTitle" style="font-family: 'Poppins', sans-serif; font-size: 14px; font-weight: 700; line-height: 150%; color: #111111;">${title}</td>
-                    </tr>
-                  </tbody></table>
-                </td>
-              </tr>
-            </tbody></table>
-            <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
-              <tbody><tr>
-                <td height="10" class="spacingHeight-10"></td>
-              </tr>
-            </tbody></table>
-            <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
-              <tbody><tr>
-                <td align="center" style="padding: 0px 40px;" class="mlContentOuter">
-                  <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="100%">
-                    <tbody><tr>
-                      <td align="center">
-                        <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left" width="267" style="width: 267px; min-width: 267px;" class="mlContentTable marginBottom">
-                          <tbody><tr>
-                            <td align="left" class="bodyTitle" id="footerText-44" style="font-family: 'Poppins', sans-serif; font-size: 12px; line-height: 150%; color: #111111;">
-                              ${address()}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td height="25" class="spacingHeight-20"></td>
-                          </tr>
-                          <tr>
-                            <td align="center">
-                              <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="left">
-                                <tbody>
-                                <tr>
-                                ${socialPanel}
-                                </tr>
-                              </tbody></table>
-                            </td>
-                          </tr>
-                        </tbody></table>
-                        <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="right" width="267" style="width: 267px; min-width: 267px;" class="mlContentTable">
-                          <tbody><tr>
-                            <td align="right" class="bodyTitle" id="footerUnsubscribeText-44" style="font-family: 'Poppins', sans-serif; font-size: 12px; line-height: 150%; color: #111111;">
-                              ${description()}
-                            </td>
-                          </tr>
-                          <tr>
-                            <td height="10"></td>
-                          </tr>
-                          <tr>
-                            <td align="right" class="bodyTitle" style="font-family: 'Poppins', sans-serif; font-size: 12px; line-height: 150%; color: #111111;">
-                             ${createUnsubscribe(unsubscribe)}
-                            </td>
-                          </tr>
-                        </tbody></table>
-                      </td>
-                    </tr>
-                  </tbody></table>
-                </td>
-              </tr>
-            </tbody></table>
-            <table role="presentation" cellpadding="0" cellspacing="0" border="0" align="center" width="640" style="width: 640px; min-width: 640px;" class="mlContentTable">
-              <tbody><tr>
-                <td height="40" class="spacingHeight-40" style="line-height: 40px; min-height: 40px;"></td>
-              </tr>
-            </tbody></table>
-          </td>
-        </tr>
-      </tbody></table>
-    </td>
-  </tr>
-</tbody></table>`;
-
+  return '123';
+  
 
 };
 // Footer content end
