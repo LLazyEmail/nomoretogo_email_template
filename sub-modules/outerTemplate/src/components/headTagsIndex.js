2b268b924cee0777a3a086024f45b48b42f63dcf
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-04 03:54:55 +0300

    Update headTagsIndex.js

--- sub-modules/outerTemplate/src/components/headTagsIndex.js
+++ sub-modules/outerTemplate/src/components/headTagsIndex.js
@@ -1,1 +1,40 @@
-xxx
+import {
+  blobHead,
+  bodyStyle,
+  font1,
+  font2,
+  headMetaTags,
+  ifStyle,
+  mediaStyle,
+  style,
+  style2,
+  headComponent,
+} 
+
+// Head params
+// const HeadParams = {
+//   blobHead: blobHead(),
+//   bodyStyle: bodyStyle(),
+//   font1: font1(),
+//   font2: font2(),
+//   headMetaTags: headMetaTags(),
+//   ifStyle: ifStyle(),
+//   mediaStyle: mediaStyle(),
+//   style: style(),
+//   style2: style2(),
+
+// };
+
+
+
+//     ${meta}
+//     ${font1}
+//     ${ifStyle}
+//     ${style}
+//     ${bodyStyle}
+//     ${mediaStyle}
+
+
+const pre_tags = () => {}
+
+const post_tags = () => {}
