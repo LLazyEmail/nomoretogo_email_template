c4d759e0fc3d06a9e9b1c76c8053779dcd5cfd92
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-23 21:19:11 +0200

    Update 04_main.test.js

--- tests/04_main.test.js
+++ tests/04_main.test.js
@@ -1,1 +1,11 @@
 import { MainHTMLString } from '../src/display/displayMain';
+
+
+describe('main display method must be initialized', () => {
+  
+    test('display main', () => {
+    expect(typeof MainHTMLString === 'string').toBe(true);
+  });
+  
+  
+});
