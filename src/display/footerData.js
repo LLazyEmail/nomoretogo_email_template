39601328e25e0778c65f78e2ad2604c0b68b164b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-24 00:01:42 +0200

    Create footerData.js

---
+++ src/display/footerData.js
@@ -0,0 +1,23 @@
+
+
+const imagePath = 'https://raw.githubusercontent.com/LLazyEmail/nomoretogo_email_template/main/data/images/amazon.png';
+
+
+
+var params = {
+  topTitle: 'No More To-Go Facebook Users:',
+  // topLinkHref: '123',
+  topLinkId: 'fb-group',
+  topLinkText: 'Facebook Group Page',
+  bottomTitle: 'Have Questions?',
+  // bottomLinkHref: '123',
+  bottomLinkId: 'email-id',
+  bottomLinkText: '"How Can We Help',
+  // bottomTextContact: 'bottomTextContact',
+};
+
+
+export {
+  imagePath,
+  params
+}
