b781af53701842261db9d098110aeea3db11e6c7
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-23 21:20:14 +0200

    Update 02_body.test.js

--- tests/02_body.test.js
+++ tests/02_body.test.js
@@ -2,8 +2,14 @@ import { BodyHTMLString } from '../src/display/displayBody';
 
 
 
-describe('main display method must be initialized', () => {
+describe('body display method must be initialized', () => {
   
-   
+   // test('display body', () => {
+  //   expect(BodyHTMLString === true).toBe(true);
+  // });
+
+  test('display body', () => {
+    expect(typeof BodyHTMLString === 'string').toBe(true);
+  });
   
 });
