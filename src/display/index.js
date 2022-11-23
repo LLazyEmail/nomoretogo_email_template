b0b4f152f86650915327105ecb63f3dd6d4b8d79
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-23 21:35:08 +0200

    Update index.js

--- src/display/index.js
+++ src/display/index.js
@@ -1,1 +1,9 @@
-xxx
+import HeadString from '../src/display/displayHead';
+import contentString from '../src/display/displayContent';
+import FooterString from '../src/display/displayFooter';
+import BodyComponentString from '../src/display/displayBodyComponent';
+
+import { MainHTMLTemplate } from '../src/display/displayMain';
+
+export {
+}
