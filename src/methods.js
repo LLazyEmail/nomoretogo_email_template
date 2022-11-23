6e5ef6afd6a21861b427fa7d0d2f14047eed2940
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-23 22:20:26 +0200

    Update methods.js

--- src/methods.js
+++ src/methods.js
@@ -1,4 +1,5 @@
-import { writeHTML, generateTemplateName } from 'markup-generator';
+//import { writeHTML, generateTemplateName } from 'markup-generator';
+
 import HeadString from '../src/display/displayHead';
 import contentString from '../src/display/displayContent';
 import FooterString from '../src/display/displayFooter';
@@ -7,16 +8,6 @@ import BodyComponentString from '../src/display/displayBodyComponent';
 import { MainHTMLTemplate } from '../src/display/displayMain';
 
 // console.log(HeadString)
-
-const catchErrorTraceOutput = (error) => {
-  // we need to test how it actually work
-  const callerLine = error.stack.split('\n')[4];
-  const index = callerLine.indexOf('at ');
-  // eslint-disable-next-line no-unused-vars
-  const clean = callerLine.slice(index + 2, callerLine.length);
-
-  throw error;
-};
 
 const printHead = () => {
   return HeadString;
@@ -38,21 +29,6 @@ const printBody = () => {
   return BodyComponentString;
 };
 
-const writingFile = (content) => {
-  if (!content) {
-    throw new Error('no content was passed into writingFile method');
-  }
-
-  const fileName = generateTemplateName('nmtg');
-
-  try {
-    writeHTML(fileName, content);
-  } catch (err) {
-    throw new Error(err.message);
-    // console.log(err);
-    //console.log(content);
-  }
-};
 
 export {
   printHead,
@@ -60,6 +36,4 @@ export {
   printMain,
   printFooter,
   printBody,
-  writingFile,
-  catchErrorTraceOutput,
 };
