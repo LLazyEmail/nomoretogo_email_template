ac47bd837cf51826aa8ab42f535f6f93979892b7
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-23 23:58:01 +0200

    Update methods.js

--- src/methods.js
+++ src/methods.js
@@ -1,5 +1,3 @@
-//import { writeHTML, generateTemplateName } from 'markup-generator';
-
 import HeadString from '../src/display/displayHead';
 import contentString from '../src/display/displayContent';
 import FooterString from '../src/display/displayFooter';
@@ -7,33 +5,3 @@ import BodyComponentString from '../src/display/displayBodyComponent';
 // import tempTest from '../src/display/displayTempTest';
 import { MainHTMLTemplate } from '../src/display/displayMain';
 
-// console.log(HeadString)
-
-// const printHead = () => {
-//   return HeadString;
-// };
-
-// const printContent = () => {
-//   return contentString;
-// };
-
-// const printMain = () => {
-//   return MainHTMLTemplate;
-// };
-
-// const printFooter = () => {
-//   return FooterString;
-// };
-
-// const printBody = () => {
-//   return BodyComponentString;
-// };
-
-
-export {
-//   printHead,
-//   printContent,
-//   printMain,
-//   printFooter,
-//   printBody,
-};
