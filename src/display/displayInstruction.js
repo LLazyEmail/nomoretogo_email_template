dfef67fe3cb84f6471d465882f79554fa611a4c5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-25 03:52:01 +0200

    Update displayInstruction.js

--- src/display/displayInstruction.js
+++ src/display/displayInstruction.js
@@ -2,16 +2,18 @@ import instructionComponent from '../components/instructionComponent';
 
 import createComponent from './createComponentUniversal';
 
+import instructionComponentParams from './_instructionData';
+
 /* ******************** DATA START **************************************** */
 // Title params
-const instructionComponentParams = {
-  title: 'Ingredients',
-  text: `Saffron: Gives traditional Tandoori its famous yellow coloring. 
-  This week we are using it in the marinade for the Yogurt Spiced Chicken. 
-  It can be pretty expensive, so turmeric is a good alternative, providing the same color and very comparable taste.`,
-  title2: 'Weekend Prep',
-  text2: '123',
-};
+// const instructionComponentParams = {
+//   title: 'Ingredients',
+//   text: `Saffron: Gives traditional Tandoori its famous yellow coloring. 
+//   This week we are using it in the marinade for the Yogurt Spiced Chicken. 
+//   It can be pretty expensive, so turmeric is a good alternative, providing the same color and very comparable taste.`,
+//   title2: 'Weekend Prep',
+//   text2: '123',
+// };
 
 /* ******************** DATA END **************************************** */
 
