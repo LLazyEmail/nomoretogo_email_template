757a9d9ce11cdf187d01f916bac4c5a9d3a5b93d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-11-25 03:49:04 +0200

    Update _instructionData.js

--- src/display/_instructionData.js
+++ src/display/_instructionData.js
@@ -1,1 +1,12 @@
-xxx
+// Title params
+const instructionComponentParams = {
+  title: 'Ingredients',
+  text: `Saffron: Gives traditional Tandoori its famous yellow coloring. 
+  This week we are using it in the marinade for the Yogurt Spiced Chicken. 
+  It can be pretty expensive, so turmeric is a good alternative, providing the same color and very comparable taste.`,
+  title2: 'Weekend Prep',
+  text2: '123',
+};
+
+
+export default instructionComponentParams;
