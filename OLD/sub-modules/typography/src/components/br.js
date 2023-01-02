6238ba944651f5d1652e898d45e53a6d82f401d6
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-02 10:36:22 +0000

    clean up

--- OLD/sub-modules/typography/src/components/br.js
+++
@@ -1,8 +0,0 @@
-import {paragraphComponent} from 'html-typography-tags';
-
-const brComponent = (attributes) => {
-    const content = '';
-    return paragraphComponent({attributes, content});
-}
-
-export default brComponent;
