04ae5c0e60f718e325133d4a0abb3cd90fbfa547
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-02 10:17:03 +0000

    clean up

---
+++ MERGE/typography/tests/index.test.js
@@ -0,0 +1,13 @@
+const buttonComponent2 = require('../src/components/button2');
+// import { buttonComponent2 } from 'nmtg-template-mailerlite-typography';
+
+describe('testing complex links', () => {
+  test('rendering head component', () => {
+    console.log(buttonComponent2);
+    //buttonComponent2({id: '12', href: 'google.com'});
+    //         const string1 = printHead();
+    //   //       // console.log(string);
+    //         expect(printHead()).toBeDefined();
+    //         writingFile(string1);
+  });
+});
