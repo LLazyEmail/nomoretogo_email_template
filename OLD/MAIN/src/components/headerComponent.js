a1a518d78a2039eb3bb9587a86c139fdd87139e4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-02 12:02:32 +0200

    Update headerComponent.js

--- OLD/MAIN/src/components/headerComponent.js
+++ OLD/MAIN/src/components/headerComponent.js
@@ -1,3 +1,6 @@
+// we have copy of this component @ innerTemplate folder
+// https://github.com/LLazyEmail/nomoretogo_email_template/issues/429
+// 
 import createComponent from '../display/createComponentUniversal';
 
 //------------------
