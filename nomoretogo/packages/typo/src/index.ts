2948cac2007f2a73c7706abf010de4a018692a5e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-05 12:58:41 +0000

    moving typo

--- nomoretogo/packages/typo/src/index.ts
+++ nomoretogo/packages/typo/src/index.ts
@@ -1,13 +1,49 @@
-import buttonComponent from './components/button';
 
+ import buttonComponent from './components/button';
 
-import linkComponent from './components/link';
+ import divComponent from './components/div';
+ 
+ 
+//  import heading from './components/heading-nmtg';
+ 
+//  import headingComponent from './components/heading';
+ 
+//  import headingOneComponent from './components/headingOne';
+ 
+ import imageComponent from './components/image';
+ 
+ import imageLink from './components/imageLink';
+ 
+ import italicComponent from './components/italic';
+ 
+ import linkComponent from './components/link';
+ 
+ 
+ import listComponent from './components/list';
+ import listItemComponent from './components/listItem';
+ 
+ import paragraphComponent from './components/paragraph';
+ 
+ import strongComponent from './components/strong';
+
+ 
+ 
+function multiply(a:number, b:number) {
+    return a * b;
+}
 
 
 export {
- 
+    multiply,
+    //------------
     buttonComponent,
-    
+    divComponent,
+    imageComponent,
+    imageLink,
+    italicComponent,
     linkComponent,
-    
+    listComponent,
+    listItemComponent,
+    paragraphComponent,
+    strongComponent,
 }
