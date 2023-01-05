2d1f749c29a7ef1163710c249d6353ba66e4c4db
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-05 16:05:16 +0000

    upload

---
+++ nomoretogo/packages/05-nmtg-typo/typography/src/components/br.js
@@ -0,0 +1,10 @@
+import paragraphComponent from 'html-typography-tags';
+
+const brComponent = (attributes) => {
+    const content = '';
+    // const { attributes, content } = params;
+
+    return paragraphComponent({attributes, content});
+}
+
+export default brComponent;
---
+++ nomoretogo/packages/05-nmtg-typo/typography/src/index.js
@@ -0,0 +1,35 @@
+import buttonComponentOld from './components/button';
+import heading from './components/heading';
+
+import strong from './components/strong';
+import { renderSubHeading, spanHeadingBlock } from './components/subheading';
+//--------
+import paragraphComponent from './components/paragraph';
+import linkComponent from './components/link';
+
+import separatorComponent from './components/separator';
+
+import {
+    imageComponent,
+    imgWrapper
+} from './components/image';
+
+
+buttonComponentOld({id: '12', href: 'google.com', text: 'GoooGLE link'});
+
+console.log(buttonComponentOld);
+
+
+export { 
+    buttonComponentOld, 
+    heading,  
+    strong, 
+    renderSubHeading,
+    spanHeadingBlock,
+    //------
+    imageComponent,
+    imgWrapper,
+    paragraphComponent,
+    linkComponent,
+    separatorComponent
+};
