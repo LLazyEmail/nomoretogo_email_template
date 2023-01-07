45ee12205ce4bc99702e51ddc9dfbada17cffc71
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-07 13:02:46 +0000

    321

--- nomoretogo/bash/build.sh
+++ nomoretogo/bash/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-npx nx build typo && npx nx build nmtg-typo && npx nx build innerTemplate && npx nx build outer
+npx nx build typo && npx nx build nmtg-typo && npx nx build innerTemplate && npx nx build outer && npx nx build sections && npx nx build display
