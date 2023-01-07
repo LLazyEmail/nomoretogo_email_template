ed8528cdee20c226a911a3b19d6dbbe1a1b87531
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-07 11:46:34 +0000

    231

--- nomoretogo/packages/outer/src/components/unsubscribeLink.ts
+++ nomoretogo/packages/outer/src/components/unsubscribeLink.ts
@@ -1,14 +1,13 @@
 import {
-    linkComponent,
-    spanHeadingBlock
-} from 'nmtg-template-mailerlite-typography';
-
+    linkComponent
+} from 'typo';
+import { spanHeadingBlock } from 'nmtg-typo';
 
 // Unsubscribe
-const createUnsubscribe = (href) => {
-    if (href == '') {
-      error.add('No unsubscribe link provided');
-    }
+const createUnsubscribe = (href:string) => {
+    // if (href == '') {
+    //   error.add('No unsubscribe link provided');
+    // }
   
     const linkParams = {
       attributes: `href="${href}" style="color: #111111; text-decoration: underline;"`,
