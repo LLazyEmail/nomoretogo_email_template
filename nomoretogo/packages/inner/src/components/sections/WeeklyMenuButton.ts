03639349627413ce9f5f5afed40d6bd0e459a710
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-21 16:14:14 +0200

    Update WeeklyMenuButton.ts

--- nomoretogo/packages/inner/src/components/sections/WeeklyMenuButton.ts
+++ nomoretogo/packages/inner/src/components/sections/WeeklyMenuButton.ts
@@ -1,19 +1,24 @@
-// TODO move Weekly menu button section into sub-components
-// const contentSubComponent = (text, recipeRowBlockRes, contentButtonParams) => {
+import {
+  buttonComponent,
+  separatorComponent,
+} from 'typo';
 
-//     // weekly menu button
-//     const buttonHTML = buttonComponent(contentButtonParams);
+// TODO move Weekly menu button section into sub-components
+const contentSubComponent = (text, recipeRowBlockRes, contentButtonParams) => {
+
+    // weekly menu button
+    const buttonHTML = buttonComponent(contentButtonParams);
   
-//     const contentButtonBlockHTML = contentButtonBlock(buttonHTML);
+    const contentButtonBlockHTML = contentButtonBlock(buttonHTML);
     
-//     const separatorHTML = separatorComponent();
-//     // All content
-//     const allContent = 
-//         text +
-//         recipeRowBlockRes + 
-//         contentButtonBlockHTML + 
-//         separatorHTML;
+    const separatorHTML = separatorComponent();
+    // All content
+    const allContent = 
+        text +
+        recipeRowBlockRes + 
+        contentButtonBlockHTML + 
+        separatorHTML;
   
-//     return allContent;
+    return allContent;
   
-//   }
+  }
