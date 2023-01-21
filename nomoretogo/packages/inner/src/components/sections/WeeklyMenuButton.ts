37811d9fba5592567c427bc7341cdd11fb03c0f1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-21 16:13:14 +0200

    Create WeeklyMenuButton.ts

---
+++ nomoretogo/packages/inner/src/components/sections/WeeklyMenuButton.ts
@@ -0,0 +1,19 @@
+// TODO move Weekly menu button section into sub-components
+// const contentSubComponent = (text, recipeRowBlockRes, contentButtonParams) => {
+
+//     // weekly menu button
+//     const buttonHTML = buttonComponent(contentButtonParams);
+  
+//     const contentButtonBlockHTML = contentButtonBlock(buttonHTML);
+    
+//     const separatorHTML = separatorComponent();
+//     // All content
+//     const allContent = 
+//         text +
+//         recipeRowBlockRes + 
+//         contentButtonBlockHTML + 
+//         separatorHTML;
+  
+//     return allContent;
+  
+//   }
