cbb6010118554bd7c53a87a1611bc5a497e38c8a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-21 16:13:32 +0200

    Update recipesSectionComponent.ts

--- nomoretogo/packages/sections/src/components/recipesSectionComponent.ts
+++ nomoretogo/packages/sections/src/components/recipesSectionComponent.ts
@@ -33,28 +33,6 @@ export const listToMatrix = <T>(list: T[], width: number): T[][] => {
   return matrix;
 };
 
-
-
-  // TODO move Weekly menu button section into sub-components
-// const contentSubComponent = (text, recipeRowBlockRes, contentButtonParams) => {
-
-//     // weekly menu button
-//     const buttonHTML = buttonComponent(contentButtonParams);
-  
-//     const contentButtonBlockHTML = contentButtonBlock(buttonHTML);
-    
-//     const separatorHTML = separatorComponent();
-//     // All content
-//     const allContent = 
-//         text +
-//         recipeRowBlockRes + 
-//         contentButtonBlockHTML + 
-//         separatorHTML;
-  
-//     return allContent;
-  
-//   }
-
   
 
    // block 2
