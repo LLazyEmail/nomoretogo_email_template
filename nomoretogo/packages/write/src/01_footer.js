26cd1cd9d73d5cc9ea2a29abe4cb800306cb5d87
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:58:50 +0200

    Update 01_footer.js

--- nomoretogo/packages/write/src/01_footer.js
+++ nomoretogo/packages/write/src/01_footer.js
@@ -2,13 +2,7 @@ import { writingFile } from 'markup-generator';
 
 import { FooterHTMLString } from 'display';
 
+import writeFileParticle from 'utils';
 
-const partialWrite2 = (string, suffix) => {
-
-  writingFile(string, suffix);
-  // TODO add a function that will display an output in console.  
-  return true;
-}
-
-export default partialWrite2(FooterHTMLString, 'nmtg-footer')
+export default writeFileParticle(FooterHTMLString, 'nmtg-footer')
 
