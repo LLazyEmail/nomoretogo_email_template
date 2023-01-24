e749d1e04ca82ece7f7f8cb9c4f8ca6570ed7b67
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 19:36:44 +0200

    Update index.ts

--- nomoretogo/packages/inner/src/index.ts
+++ nomoretogo/packages/inner/src/index.ts
@@ -7,15 +7,17 @@ import contentSubTitleText from './components/contentSubTitleText';
 // import contentTableWithTwoRecipes from './components/contentTableWithTwoRecipes';
 import contentTextDescription from './components/contentTextDescription';
 import contentTitleText from './components/contentTitleText';
-
+//---------------------------
 import instructionTitle from './components/instruction/instructionTitle';
 import instructionText from './components/instruction/instructionText';
-
+//--------------------------
 import lineBlock from './components/lineBlock';
-//----------------------
-import recipeComponent from './components/recipe/recipeComponent';
 
-import recipeRowBlock from './components/recipe/recipesRowBlock';
+
+//----------------------
+// removing other components related to recipes
+import recipesSectionComponent from './components/recipe';
+
 //----------------------
 import viewInBrowsLink from './components/header/viewBrowserLink';
 
@@ -32,8 +34,6 @@ export {
   instructionText,
   lineBlock,
   //--------
-  recipeComponent,
-  recipeRowBlock,
   viewInBrowsLink
 };
 
