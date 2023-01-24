b45f0dd569548f2ec7909ef12df3908358e59a22
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:33:20 +0200

    Update 03_content.js

--- nomoretogo/packages/write/src/03_content.js
+++ nomoretogo/packages/write/src/03_content.js
@@ -1,7 +1,14 @@
 import { writingFile } from 'markup-generator';
 
-import ContentHTMLString from 'display';
+import { ContentHTMLString } from 'display';
 
+const partialWrite3 = (string, suffix) => {
 
-      writingFile(ContentHTMLString, 'nmtg-content');
+  writingFile(string, suffix);
+  // TODO add a function that will display an output in console.  
+      return true;
+}
+
+export default partialWrite3(ContentHTMLString, 'nmtg-content')
+
 
