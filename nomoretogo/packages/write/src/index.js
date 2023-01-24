b8d95e9b12231c254897b9b94f7f11818b7252b0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 19:00:45 +0200

    Update index.js

--- nomoretogo/packages/write/src/index.js
+++ nomoretogo/packages/write/src/index.js
@@ -4,3 +4,5 @@ import bodyFlag from '02_body';
 import contentFlag from '03_content';
 import recipesFlag from '04_recipes';
 import mainFlag from '05_main';
+
+// depends on what we are calling in CLI - we need to be able to run a separate sets of methods
