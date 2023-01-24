5ea6b05ab43e7b6db8aba4ed8a11cef5293fb92d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 19:30:32 +0200

    Update and rename cRecipesSectionComponent.ts to section.ts

--- nomoretogo/packages/inner/src/components/recipe/section.ts
+++ nomoretogo/packages/inner/src/components/recipe/section.ts
@@ -3,33 +3,12 @@ import {
     recipeRowBlock
   } from 'innerTemplate';
 
-// usage
-// var matrix = listToMatrix([1, 2, 3, 4, 4, 5, 6, 7, 8, 9], 3);
 
-/**
- * Convert simple array into two-dimensional array (matrix)
- *
- * @param list The array
- * @param width The num of elements per sub-array
- * @return the new matrix
- */
-export const listToMatrix = <T>(list: T[], width: number): T[][] => {
-  const matrix = [];
+import { listToMatrix } from 'utils';
+// listToMatrix moved to utils
 
-  for (let i = 0, k = -1; i < list.length; i++) {
-      if (i % width === 0) {
-          k++;
-          matrix[k] = [];
-      }
-      matrix[k].push(list[i]);
-  }
 
-  return matrix;
-};
-
-  
-
-   // block 2
+// block 2
 const createRecipesPanel = (recipes) => {
 
   // var recipeHTMLMarkupArray
