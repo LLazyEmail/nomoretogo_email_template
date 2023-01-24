1898fbf878bd9074190534ec3bf257a7e6763843
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 19:34:14 +0200

    Update displayRecipes.js

--- nomoretogo/packages/display/src/display/displayRecipes.js
+++ nomoretogo/packages/display/src/display/displayRecipes.js
@@ -1,18 +1,8 @@
-import recipesSectionComponent from '../components/recipesSectionComponent';
+import { recipesSectionComponent } from 'inner';
 
 import { recipes } from '../config';
 
 
-import { buttonComponent2 } from 'html-typography-tags';
-
-// const contentButtonParams = {
-//   id: '04-22-2021',
-//   href: 'https://www.nomoretogo.com/weekly-menu/',
-//   text: '123',
-// };
-const config = {id: '12', href: 'google.com'};
-
-console.log(buttonComponent2(config));
 
 var RecipesHTMLString = null;
 
