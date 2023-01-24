93597c7815483511696a166e75253da02b44ad05
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:35:03 +0200

    Update index.js

--- nomoretogo/packages/write/src/index.js
+++ nomoretogo/packages/write/src/index.js
@@ -1,3 +1,6 @@
 import headFlag from '00_head';
 import footerFlag from '01_footer';
 import bodyFlag from '02_body';
+import contentFlag from '03_content';
+import recipesFlag from '04_recipes';
+import mainFlag from '05_main';
