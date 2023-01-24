8de71218f0f0c5f2eb1a02d6eead2512bb3ce3df
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:34:08 +0200

    Update 04_recipes.js

--- nomoretogo/packages/write/src/04_recipes.js
+++ nomoretogo/packages/write/src/04_recipes.js
@@ -1,6 +1,12 @@
 import { writingFile } from 'markup-generator';
 
-import RecipesHTMLString from 'display';
+import { RecipesHTMLString } from 'display';
 
+const partialWrite3 = (string, suffix) => {
 
-      writingFile(RecipesHTMLString, 'nmtg-recipe');
+writingFile(string, suffix);
+// TODO add a function that will display an output in console.  
+return true;
+}
+
+export default partialWrite3(RecipesHTMLString, 'nmtg-recipe')
