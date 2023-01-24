63cd4c4fcb5f70434eefc30cc64275f18f8b6ad6
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:32:07 +0200

    Update 02_body.js

--- nomoretogo/packages/write/src/02_body.js
+++ nomoretogo/packages/write/src/02_body.js
@@ -7,7 +7,7 @@ const partialWrite3 = (string, suffix) => {
 
   writingFile(string, suffix);
   // TODO add a function that will display an output in console.  
-
+return true;
 }
 
 export default partialWrite3(BodyHTMLString, 'nmtg-Body')
