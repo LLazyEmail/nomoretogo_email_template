7268461034b07a93d22db495c6673667303fb24b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:31:28 +0200

    Update 00_head.js

--- nomoretogo/packages/write/src/00_head.js
+++ nomoretogo/packages/write/src/00_head.js
@@ -7,7 +7,7 @@ const partialWrite1 = (string, suffix) => {
 
   writingFile(string, suffix);
   // TODO add a function that will display an output in console.  
-
+  return true;
 }
 
 export default partialWrite1(HeadHTMLString, 'nmtg-Head');
