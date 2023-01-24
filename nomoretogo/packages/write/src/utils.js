6b233ce6ae3be15adadfe5dfc0aa5d1ea583aa33
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:53:16 +0200

    Create utils.js

---
+++ nomoretogo/packages/write/src/utils.js
@@ -0,0 +1,8 @@
+const partialWrite3 = (string, suffix) => {
+
+  writingFile(string, suffix);
+  // TODO add a function that will display an output in console.  
+  return true;
+}
+
+export default writeFileParticle;
