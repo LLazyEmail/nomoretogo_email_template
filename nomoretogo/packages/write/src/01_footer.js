ff9842d78575a003547833336cec0a463cbf40e1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-24 18:31:58 +0200

    Update 01_footer.js

--- nomoretogo/packages/write/src/01_footer.js
+++ nomoretogo/packages/write/src/01_footer.js
@@ -7,7 +7,7 @@ const partialWrite2 = (string, suffix) => {
 
   writingFile(string, suffix);
   // TODO add a function that will display an output in console.  
-
+  return true;
 }
 
 export default partialWrite2(FooterHTMLString, 'nmtg-footer')
