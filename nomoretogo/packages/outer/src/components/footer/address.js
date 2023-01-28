6cde95dc72c42edbcf499636a9973679b9c65eae
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-28 17:35:17 +0200

    Create address.js

---
+++ nomoretogo/packages/outer/src/components/footer/address.js
@@ -0,0 +1,8 @@
+// Address
+const address = () => {
+  const addressParams = {
+    attributes: `style="margin-top: 0px; margin-bottom: 0px;"`,
+    content: 'Dallas,&nbsp;Texas United States',
+  };
+  return paragraphComponent(addressParams);
+};
