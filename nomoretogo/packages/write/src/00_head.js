5f829f82927ec2ecabe802c4e9219f949707bb79
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-28 12:45:39 +0000

    213

--- nomoretogo/packages/write/src/00_head.js
+++ nomoretogo/packages/write/src/00_head.js
@@ -1,4 +1,4 @@
-// import { writingFile } from 'markup-generator';
+// TODO combine all methods into one file, as they looks very simple right now.
 import { HeadHTMLString } from 'display';
 
 import writeFileParticle from 'utils';
