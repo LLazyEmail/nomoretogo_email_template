dd1ca2b58894b48c2985bcf654ef4762825645d3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-01-28 16:46:13 +0200

    Update text.js

--- nomoretogo/packages/inner/src/components/instruction/text.js
+++ nomoretogo/packages/inner/src/components/instruction/text.js
@@ -14,3 +14,16 @@ export default function (instructionText) {
 
   return instructionTextMainBlock(instructionText);
 }
+// replacing 
+// return `<p style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;">${text}</p>`;
+const createText = (text) => {
+  
+  const content = { content: text };
+
+  const config = {
+    attributes: `style="margin-top: 0px; margin-bottom: 10px; line-height: 150%;"`,
+    content,
+  };
+  return paragraphComponent(config);
+
+};
