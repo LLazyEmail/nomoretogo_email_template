ca4084f70583aadb55fa1f7835f95d227eac23e1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-02-14 16:41:04 +0000

    сдуфт гз

--- 102 NEW/SUB/outerTemplate/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import { footerComponent } from "../src/index";
-
-
-describe('testing our functions', () => {
-  test('', () => {
-    expect(typeof footerComponent === 'function').toBe(true);
-  });
-});
