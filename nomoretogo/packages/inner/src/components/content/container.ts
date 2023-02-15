ae7c298c5da11a726173929a35eb3e367f3dd2fe
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-02-15 14:07:06 +0000

    content

---
+++ nomoretogo/packages/inner/src/components/content/container.ts
@@ -0,0 +1,12 @@
+import titleTextMainBlock from './title';
+import subTitleMainBlock from './subtitle';
+import textDescriptionMainBlock from './description';
+
+
+const contentContainer = () => {
+
+
+
+    
+
+}
