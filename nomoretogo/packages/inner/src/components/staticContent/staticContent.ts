c4dc8cc75655db890774948fb432ace52e4c786a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-02-17 11:20:13 +0000

    123

--- nomoretogo/packages/inner/src/components/staticContent/staticContent.ts
+++ nomoretogo/packages/inner/src/components/staticContent/staticContent.ts
@@ -1,79 +1,61 @@
 import { 
-  separatorComponent, 
-  buttonComponent 
+  separatorComponent
 } from 'nmtg-typo';
 
 import contentContainer from '../content/container';
 
-// import { recipes } from '../../config';
-// import recipeRowBlock from '../recipe/row';
+
 // import subtitle from '../content/subtitle';
 // import textDescriptionMainBlock from '../content/description';
 // import titleTextMainBlock from '../content/title';
 
-
-
 import WeeklyMenuContainerComponent from '../weekly-menu/WeeklyMenuComponent';
-
-
-// const buttonParams = {
-//   id: '04-22-2021',
-//   href: 'https://www.nomoretogo.com/weekly-menu/',
-//   text: `Get This Week's Menu`,
-// };
-
-
-// import recipeComponent from '../recipe/recipe';
-// import buttonBlock from '../weekly-menu/WeeklyMenuButtonBlock';
-
-
 
 import instructionContainerStatic from '../instruction/containerStatic';
 
 
+const StaticContentComponent = (params) => {
+  // distructure params
+  const {
+    title,
+    subtitle,
+    description,
+    // contentRecipeParams,
 
-  const StaticContentComponent = (params) => {
-    // distructure params
-    const {
-      title,
-      subtitle,
-      description,
-      contentRecipeParams,
+    // dataImage,
+    // contentButtonParams,
 
-      dataImage,
-      contentButtonParams,
+    recipes,
+    buttonParams
+  } = params;
 
-      recipes,
-      buttonParams
-    } = params;
+
+  // block 1
+  const text = contentContainer({
+    title, subtitle, description
+  })
+  // const text =
+  //   contentTitleTextRes + contentSubTitleTextRes + contentTextDescriptionRes;
+  // const text = '<!--  TEXT -->';
+
+
+
+  // block 2
+  const weeklyHTML = WeeklyMenuContainerComponent(recipes, buttonParams);
+  const separator = separatorComponent();
+
+
+  const contentAndAfter = weeklyHTML +  separator;
+
+
+  const instrHTML = instructionContainerStatic();
+  console.log(instrHTML);
+
+  // All content
+  const allContent = text + contentAndAfter;
+  return allContent;
   
-
-    // block 1
-    const text = contentContainer({
-      title, subtitle, description
-    })
-
-
-    // const text =
-    //   contentTitleTextRes + contentSubTitleTextRes + contentTextDescriptionRes;
-  
-    // const text = '<!--  TEXT -->';
-
-
-
-    // block 2
-    const weeklyHTML = WeeklyMenuContainerComponent(recipes, buttonParams);
-    const separator = separatorComponent();
-
-
-    const contentAndAfter = weeklyHTML +  separator;
-  
-
-    // All content
-    const allContent = text + contentAndAfter;
-    return allContent;
-    
-  };
+};
   
   export default StaticContentComponent;
 
