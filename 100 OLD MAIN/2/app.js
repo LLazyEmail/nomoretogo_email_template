ad4f9083838b4a3424bd4b6e9fa7ee7c5da97d3c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-03-11 16:33:25 +0000

    trying all options

---
+++ 100 OLD MAIN/2/app.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const path = require("path");
+// const {
+//   generateHtmlFullTemplateHackernoon,
+//   generateReactFullTemplateHackernoon,
+//   generateHtmlFullTemplateRecipes,
+// } = require("@lazyemail/markdown-to-email");
+// var multiparty = require("multiparty");
+
+const app = express();
+
+const port = 9000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/generate", function (req, res, next) {
+//  var form = new multiparty.Form();
+
+//   let generateTo = "";
+//   let template = "";
+
+//   const file = {
+//     filename: null,
+//     bufferArray: [],
+//     totalLength: 0,
+//   };
+
+//   form.on("error", next); 
+  // form.on("close", function () {
+  //   const templateMap = {
+  //     hackernoon: {
+  //       html: generateHtmlFullTemplateHackernoon,
+  //       react: generateReactFullTemplateHackernoon,
+  //     },
+  //     recipes: {
+  //       html: generateHtmlFullTemplateRecipes,
+  //     },
+  //   };
+
+  //   bufferFile = Buffer.concat(file.bufferArray, file.totalLength);
+  //   const generator = templateMap[template][generateTo];
+
+  //   if (!generator) {
+  //     res.status(400).send('Sorry, cant find that');
+  //     return;
+  //   }
+
+  //   const content = generator(bufferFile.toString());
+
+  //   const extentionMap = {
+  //     html: {
+  //       ext: "html",
+  //       contentType: "text/html",
+  //     },
+  //     react: {
+  //       ext: "js",
+  //       contentType: "application/javascript",
+  //     },
+  //   };
+
+  //   const extentionData = extentionMap[generateTo];
+
+  //   const filenameResult = `${file.filename}.${extentionData.ext}`;
+  //   res.setHeader("Content-Type", extentionData.contentType);
+  //   res.setHeader(
+  //     "Content-Disposition",
+  //     `attachment; filename="${filenameResult}"`
+  //   );
+  //   res.setHeader("filename", file.filename);
+
+  //   res.send(content);
+  // });
+
+  // form.on("field", function (name, val) {
+  //   if (name === "generateTo") {
+  //     generateTo = val;
+  //     return;
+  //   }
+
+  //   if (name === "template") {
+  //     template = val;
+  //     return;
+  //   }
+  // });
+
+  // form.on("part", function (part) {
+  //   if (!part.filename) return;
+  //   if (part.name !== "markdownFile") return part.resume();
+  //   file.filename = part.filename.slice(0, part.filename.length - 3);
+
+  //   part.on("data", function (buf) {
+  //     file.bufferArray.push(buf);
+  //     file.totalLength += buf.length;
+  //   });
+  // });
+
+  // form.parse(req);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ 100 OLD MAIN/2/public/js/index.js
@@ -0,0 +1,59 @@
+function downloadBlob(blob, name = "file.txt") {
+  const blobUrl = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+
+  link.href = blobUrl;
+  link.download = name;
+  document.body.appendChild(link);
+
+  link.click();
+
+  document.body.removeChild(link);
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const errElement = document.getElementById("err");
+  if (errElement.innerHTML) {
+    errElement.innerHTML = "";
+    errElement.removeAttribute("class");
+  }
+
+  const myform = document.getElementById("myform");
+  const formdata = new FormData(myform);
+
+  if (!formdata.get("markdownFile").name) {
+    errElement.innerHTML = "Choose a file";
+    errElement.setAttribute("class", "error");
+    return;
+  }
+
+  fetch(`${window.origin}/generate`, {
+    method: "POST",
+    body: formdata,
+  })
+    .then((res) => {
+      console.log("res", res);
+      if (res.status === 400) {
+        throw new Error("Error");
+      }
+
+      return res
+        .blob()
+        .then((blob) => ({
+          filename: res.headers.get("filename"),
+          raw: blob,
+        }))
+        .then((result) => {
+          downloadBlob(result.raw, result.filename);
+        });
+    })
+    .catch(() => {
+      const errElement = document.getElementById("err");
+      errElement.innerHTML = "Error";
+      errElement.setAttribute("class", "error");
+    });
+
+  return false;
+}
